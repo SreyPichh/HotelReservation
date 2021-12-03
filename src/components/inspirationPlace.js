17d15a4da69acda5f43ad0863da3b5983bcbf17d
SreyPichh <phansreypich2015@gmail.com>
2021-12-03 23:42:39 +0700

    feat: inspirationPlace component

---
+++ src/components/inspirationPlace.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+class InspirationPlace extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: this.props.image}} style={styles.thumbnailImage} />
+        <Text style={styles.title}>{this.props.title}</Text>
+        <Text style={{color: 'white', fontSize: 12, fontWeight: '300', marginLeft: 10, marginTop: 5}}>{this.props.distance}</Text>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    height: 180,
+    marginTop: 30,
+    backgroundColor: '#CF4F4F',
+    borderRadius: 14,
+    marginRight: 16
+  },
+  thumbnailImage: {
+    width: 150,
+    height: 120,
+    borderTopEndRadius: 14,
+    borderTopLeftRadius: 14
+  },
+  title:{
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 14,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10
+  }
+});
+
+export default InspirationPlace;
--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,14 +1,101 @@
-import React, {Component} from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import { isTemplateElement } from '@babel/types';
+import React, {Component, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+
+import InspirationPlace from '../components/inspirationPlace';
+
+// JSON Data
+import inspirationPlaceData from '../assets/inspirationPlaceData.json';
 
 class Home extends Component {
+  // const [text, setText] = useState('');
   render() {
     return (
-      <SafeAreaView>
-        <Text>Hotel Reservation</Text>
+      <SafeAreaView style={styles.Container}>
+        <ScrollView>
+          <Text style={styles.title}>Hello, There!</Text>
+          <Text style={{marginTop: 12}}>Let's reserve a room!</Text>
+          <View style={styles.searchBar}>
+            <TextInput
+              style={styles.search}
+              placeholder="Search any Hotel!"
+              // onChangeText={text => setText(text)}
+              // defaultValue={text}
+            />
+            {/* <Text style={{padding: 10, fontSize: 26}}>{text.split('').map((word) => word && '🍕').join('')}</Text> */}
+          </View>
+          {/* popular Places */}
+          <View style={styles.places}>
+            <Text style={{fontWeight: 'bold', fontSize: 18}}>
+              Inspiration for your next trip
+            </Text>
+            <FlatList
+            
+              showsHorizontalScrollIndicator={false}
+              horizontal={true} 
+              keyboardDismissMode="handled"
+              data={inspirationPlaceData}
+              keyExtractor = {item => item.id}
+              renderItem = {({item}) => {
+                return(
+                  <>
+                  {
+                    <TouchableOpacity
+                      
+                    >
+                      <InspirationPlace 
+                     
+                        key={item.id}
+                        image={item.image}
+                        title={item.title}
+                        distance={item.sub}
+                       />
+                    </TouchableOpacity>
+                  }
+                  </>
+                )
+              }}
+            >
+            </FlatList>
+          </View>
+        </ScrollView>
       </SafeAreaView>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 26,
+  },
+  searchBar: {
+    marginTop: 16,
+  },
+  search: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#848484',
+    borderRadius: 6,
+    paddingLeft: 10,
+  },
+  places: {
+    paddingTop: 30,
+  },
+
+});
 
 export default Home;
